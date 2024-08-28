4ed8c679e4f90a8177895a8fdab13d1c79962a58
Daryl Tejares <daryltejares@outlook.com>
2024-08-28 15:01:24 +0800

    Created a to do list

--- VB_Net_Daryl/Form1.Designer.cs
+++ VB_Net_Daryl/Form1.Designer.cs
@@ -28,46 +28,158 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.equalBtn = new System.Windows.Forms.Button();
+            this.clearBtn = new System.Windows.Forms.Button();
+            this.ArithmeticBox = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.imageList2 = new System.Windows.Forms.ImageList(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
-            // button1
+            // equalBtn
             // 
-            this.button1.Location = new System.Drawing.Point(336, 221);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.equalBtn.Location = new System.Drawing.Point(350, 208);
+            this.equalBtn.Name = "equalBtn";
+            this.equalBtn.Size = new System.Drawing.Size(128, 47);
+            this.equalBtn.TabIndex = 0;
+            this.equalBtn.Text = "=";
+            this.equalBtn.UseVisualStyleBackColor = true;
+            this.equalBtn.Click += new System.EventHandler(this.OnClickEqualButton);
             // 
-            // button2
+            // clearBtn
             // 
-            this.button2.Location = new System.Drawing.Point(630, 142);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "Clear All";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
+            this.clearBtn.Location = new System.Drawing.Point(350, 62);
+            this.clearBtn.Name = "clearBtn";
+            this.clearBtn.Size = new System.Drawing.Size(128, 38);
+            this.clearBtn.TabIndex = 1;
+            this.clearBtn.Text = "Clear All";
+            this.clearBtn.UseVisualStyleBackColor = true;
+            this.clearBtn.Click += new System.EventHandler(this.ClearBtnOnClick);
+            // 
+            // ArithmeticBox
+            // 
+            this.ArithmeticBox.Location = new System.Drawing.Point(160, 62);
+            this.ArithmeticBox.Name = "ArithmeticBox";
+            this.ArithmeticBox.Size = new System.Drawing.Size(167, 20);
+            this.ArithmeticBox.TabIndex = 2;
+            this.ArithmeticBox.Text = "Enter a number here";
+            this.ArithmeticBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.ArithmeticBox.TextChanged += new System.EventHandler(this.AritmeticBox);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(350, 106);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(61, 45);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "+\r\n";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(417, 106);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(61, 45);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "-";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(350, 157);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(61, 45);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "x";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(417, 157);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(61, 45);
+            this.button6.TabIndex = 6;
+            this.button6.Text = "/";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(302, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(78, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "CALCULATOR";
+            // 
+            // imageList1
+            // 
+            this.imageList1.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit;
+            this.imageList1.ImageSize = new System.Drawing.Size(16, 16);
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // imageList2
+            // 
+            this.imageList2.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit;
+            this.imageList2.ImageSize = new System.Drawing.Size(16, 16);
+            this.imageList2.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.InitialImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.InitialImage")));
+            this.pictureBox1.Location = new System.Drawing.Point(526, 62);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(225, 193);
+            this.pictureBox1.TabIndex = 8;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoValidate = System.Windows.Forms.AutoValidate.Disable;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.ArithmeticBox);
+            this.Controls.Add(this.clearBtn);
+            this.Controls.Add(this.equalBtn);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Calculator";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button equalBtn;
+        private System.Windows.Forms.Button clearBtn;
+        private System.Windows.Forms.TextBox ArithmeticBox;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.ImageList imageList2;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
--- VB_Net_Daryl/Form1.cs
+++ VB_Net_Daryl/Form1.cs
@@ -15,11 +15,81 @@ namespace VB_Net_Daryl
         public Form1()
         {
             InitializeComponent();
+            ArithmeticBox.TextChanged += ArithmeticBox_TextChanged;
         }
 
-        private void button2_Click(object sender, EventArgs e)
+        private void ArithmeticBox_TextChanged(object sender, EventArgs e)
+        {
+            string allowedChars = "0123456789+-*/.";
+
+            string filteredText = string.Empty;
+            foreach (char ch in ArithmeticBox.Text)
+            {
+                if (allowedChars.Contains(ch))
+                {
+                    filteredText += ch;
+                }
+            }
+
+            if (ArithmeticBox.Text != filteredText)
+            {
+                ArithmeticBox.Text = filteredText;
+                ArithmeticBox.SelectionStart = ArithmeticBox.Text.Length;
+            }
+        }
+
+
+        public void AritmeticBox(object sender, EventArgs e)
+        {
+            ArithmeticBox.Enabled = true;
+        }
+
+        private void ClearBtnOnClick(object sender, EventArgs e)
+        {
+            if(ArithmeticBox.Text == String.Empty || ArithmeticBox.Text == "Enter a number here")
+            {
+                if(ArithmeticBox.Text == "Enter a number here")
+                {
+                    return;
+                } else
+                {
+                    MessageBox.Show("Input is empty, nothing to clear", "Task Complete", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                
+                return;
+            }
+            else
+            {
+                ArithmeticBox.Text = String.Empty;
+                ArithmeticBox.Enabled = false;
+            }
+            
+        }
+
+        private void button3_Click(object sender, EventArgs e)
         {
 
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            string imagePath = "/Images/calculator.png";
+            pictureBox1.Image = Image.FromFile(imagePath);
+        }
+
+        private void OnClickEqualButton(object sender, EventArgs e)
+        {
+            
         }
     }
 }
